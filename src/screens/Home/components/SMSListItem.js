c8fbf09c0bafbefa48a09f2a07f143765a93c00b
RabbitRun47 <rabbitrun47@pop-os.localdomain>
2022-04-24 00:16:31 +0530

    feat: sms list view

--- src/screens/Home/components/SMSListItem.js
+++ src/screens/Home/components/SMSListItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {format, isSameDay, isSameYear} from 'date-fns';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 export const SMSListItem = ({data}) => {
   const date = new Date(data.date);
   const currentDate = new Date();
@@ -12,18 +14,43 @@ export const SMSListItem = ({data}) => {
     }
   }
   return (
-    <View>
-      <View style={styles.header}>
-        <Text style={styles.address}>{data.address}</Text>
-        <Text style={styles.date}>{format(date, displayDateFormat)}</Text>
+    <View style={styles.container}>
+      <View style={styles.icon}>
+        <Icon size={20} name="user-alt" color={'white'} />
       </View>
-      <Text>{data.body}</Text>
+      <View style={styles.textContainer}>
+        <View style={styles.header}>
+          <Text style={styles.address}>{data.address}</Text>
+          <Text style={styles.date}>{format(date, displayDateFormat)}</Text>
+        </View>
+        <Text style={styles.body} numberOfLines={2}>
+          {data.body}
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    borderRadius: 100,
+    backgroundColor: 'orange',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 10,
+  },
+  textContainer: {flexGrow: 1},
   header: {
+    marginBottom: 5,
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
@@ -31,4 +58,5 @@ const styles = StyleSheet.create({
   },
   address: {fontSize: 15, fontWeight: '700'},
   date: {fontSize: 12},
+  body: {marginBottom: 5, flexGrow: 1, maxWidth: '90%'},
 });
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,13 +2,42 @@ import React from 'react';
 import {FlatList} from 'react-native';
 import {ReadSMSData} from '../../data';
 import {SMSListItem} from './components/SMSListItem';
+import {View, StyleSheet} from 'react-native';
 
 export const Home = () => {
+  const sortedReadSMSData = ReadSMSData.sort(
+    (sms1, sms2) => sms2.date - sms1.date,
+  );
+  const addresses = Array.from(new Set(ReadSMSData.map(sms => sms.address)));
+  const smsListData = addresses.map(address =>
+    sortedReadSMSData.find(sms => sms.address === address),
+  );
+
+  const ItemSeparaor = () => {
+    return <View style={styles.separator} />;
+  };
+
   return (
-    <FlatList
-      data={ReadSMSData}
-      keyExtractor={item => item._id}
-      renderItem={({item}) => <SMSListItem data={item} />}
-    />
+    <View style={styles.container}>
+      <FlatList
+        data={smsListData}
+        keyExtractor={item => item._id}
+        renderItem={({item}) => <SMSListItem data={item} />}
+        ItemSeparatorComponent={ItemSeparaor}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  separator: {
+    backgroundColor: 'grey',
+    opacity: 0.1,
+    height: 1,
+    width: '100%',
+    marginVertical: 5,
+  },
+});
